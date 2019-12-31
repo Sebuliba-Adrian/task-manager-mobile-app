7c23cc1f732f134a14cf8ba898b4ddd31686e944
Sebuliba-Adrian <adrian.sebuliba1@gmail.com>
2019-12-31 16:21:23 +0300

    implement todo delete method

--- App.js
+++ App.js
@@ -6,38 +6,34 @@ import {
   FlatList,
   TouchableOpacity
 } from "react-native";
+import Header from "./components/Header";
+import TodoItem from "./components/TodoItem";
 
 export default function App() {
   const [name, setName] = useState("adrian");
-  const [people, setPeople] = useState([
-    { name: "shaun", id: "1" },
-    { name: "yoshi", id: "2" },
-    { name: "maria", id: "3" },
-    { name: "luigi", id: "4" },
-    { name: "peach", id: "5" },
-    { name: "toad", id: "6" },
-    { name: "bowser", id: "7" }
+  const [todos, setTodos] = useState([
+    { text: "buy coffee", key: "1" },
+    { text: "create an app", key: "2" },
+    { text: "play on the switch", key: "3" }
   ]);
-
-  const pressHandler = id => {
-    console.log(id);
-    setPeople(prevPeople => {
-      return prevPeople.filter(person => person.id != id);
+  const pressHandler = key => {
+    setTodos(prevTodos => {
+      return prevTodos.filter(todo => todo.key != key);
     });
   };
-
   return (
     <View style={styles.container}>
-      <FlatList
-        numColumns={2}
-        keyExtractor={item => item.id}
-        data={people}
-        renderItem={({ item }) => (
-          <TouchableOpacity onPress={() => pressHandler(item.id)}>
-            <Text style={styles.item}>{item.name}</Text>
-          </TouchableOpacity>
-        )}
-      />
+      <Header />
+      <View style={styles.content}>
+        <View style={styles.list}>
+          <FlatList
+            data={todos}
+            renderItem={({ item }) => (
+              <TodoItem item={item} pressHandler={pressHandler} />
+            )}
+          />
+        </View>
+      </View>
     </View>
   );
 }
@@ -45,25 +41,12 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#fff",
-    paddingTop: 40,
-    paddingHorizontal: 20
-    // alignItems: "center",
-    //justifyContent: "center"
+    backgroundColor: "#fff"
   },
-  input: {
-    borderWidth: 1,
-    borderColor: "#777",
-    padding: 8,
-    margin: 10,
-    width: 200
+  content: {
+    padding: 40
   },
-  item: {
-    marginTop: 24,
-    padding: 30,
-    backgroundColor: "pink",
-    fontSize: 24,
-    marginHorizontal: 10,
-    marginTop: 24
+  list: {
+    marginTop: 20
   }
 });
