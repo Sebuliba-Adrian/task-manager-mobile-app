945a3dcd838558c6e08d1ac9c87c2b74fb8c5c4d
Sebuliba-Adrian <adrian.sebuliba1@gmail.com>
2019-12-31 09:01:56 +0300

    add scrollview

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { StyleSheet, Text, View, Button, TextInput } from "react-native";
+import { StyleSheet, Text, View, ScrollView } from "react-native";
 
 export default function App() {
   const [name, setName] = useState("adrian");
@@ -14,13 +14,13 @@ export default function App() {
   ]);
   return (
     <View style={styles.container}>
-      {people.map(item => {
-        return (
+      <ScrollView>
+        {people.map(item => (
           <View key={item.key}>
             <Text style={styles.item}>{item.name}</Text>
           </View>
-        );
-      })}
+        ))}
+      </ScrollView>
     </View>
   );
 }
