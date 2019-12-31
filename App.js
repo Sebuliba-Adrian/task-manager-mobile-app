dc77d2417fc904b28e2867fc1cbdb7bf61a3550f
Sebuliba-Adrian <adrian.sebuliba1@gmail.com>
2019-12-31 08:56:50 +0300

    create a list

--- App.js
+++ App.js
@@ -3,27 +3,24 @@ import { StyleSheet, Text, View, Button, TextInput } from "react-native";
 
 export default function App() {
   const [name, setName] = useState("adrian");
-  const [age, setAge] = useState(30);
-
+  const [people, setPeople] = useState([
+    { name: "shaun", key: "1" },
+    { name: "yoshi", key: "2" },
+    { name: "maria", key: "3" },
+    { name: "luigi", key: "4" },
+    { name: "peach", key: "5" },
+    { name: "toad", key: "6" },
+    { name: "bowser", key: "7" }
+  ]);
   return (
     <View style={styles.container}>
-      <Text>Enter name:</Text>
-      <TextInput
-        multiline
-        style={styles.input}
-        placeholder="e.g John Doe"
-        onChangeText={val => setName(val)}
-      />
-      <Text>Enter age:</Text>
-      <TextInput
-        keyboardType="numeric"
-        style={styles.input}
-        placeholder="e.g 99"
-        onChangeText={val => setAge(val)}
-      />
-      <Text>
-        name: {name}, age: {age}
-      </Text>
+      {people.map(item => {
+        return (
+          <View key={item.key}>
+            <Text style={styles.item}>{item.name}</Text>
+          </View>
+        );
+      })}
     </View>
   );
 }
@@ -32,8 +29,10 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center"
+    paddingTop: 40,
+    paddingHorizontal: 20
+    // alignItems: "center",
+    //justifyContent: "center"
   },
   input: {
     borderWidth: 1,
@@ -41,5 +40,11 @@ const styles = StyleSheet.create({
     padding: 8,
     margin: 10,
     width: 200
+  },
+  item: {
+    marginTop: 24,
+    padding: 30,
+    backgroundColor: "pink",
+    fontSize: 24
   }
 });
