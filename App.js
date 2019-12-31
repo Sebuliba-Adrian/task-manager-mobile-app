6c02dd092d2529275198145e84751c20b04c1eb3
Sebuliba-Adrian <adrian.sebuliba1@gmail.com>
2019-12-31 09:52:20 +0300

    allow rendering list data with flatlist component

--- App.js
+++ App.js
@@ -1,26 +1,32 @@
 import React, { useState } from "react";
-import { StyleSheet, Text, View, ScrollView } from "react-native";
+import { StyleSheet, Text, View, FlatList } from "react-native";
 
 export default function App() {
   const [name, setName] = useState("adrian");
   const [people, setPeople] = useState([
-    { name: "shaun", key: "1" },
-    { name: "yoshi", key: "2" },
-    { name: "maria", key: "3" },
-    { name: "luigi", key: "4" },
-    { name: "peach", key: "5" },
-    { name: "toad", key: "6" },
-    { name: "bowser", key: "7" }
+    { name: "shaun", id: "1" },
+    { name: "yoshi", id: "2" },
+    { name: "maria", id: "3" },
+    { name: "luigi", id: "4" },
+    { name: "peach", id: "5" },
+    { name: "toad", id: "6" },
+    { name: "bowser", id: "7" }
   ]);
   return (
     <View style={styles.container}>
-      <ScrollView>
+      <FlatList
+        numColumns={3}
+        keyExtractor={item => item.id}
+        data={people}
+        renderItem={({ item }) => <Text style={styles.item}>{item.name}</Text>}
+      />
+      {/* <ScrollView>
         {people.map(item => (
           <View key={item.key}>
             <Text style={styles.item}>{item.name}</Text>
           </View>
         ))}
-      </ScrollView>
+      </ScrollView> */}
     </View>
   );
 }
@@ -45,6 +51,8 @@ const styles = StyleSheet.create({
     marginTop: 24,
     padding: 30,
     backgroundColor: "pink",
-    fontSize: 24
+    fontSize: 24,
+    marginHorizontal: 10,
+    marginTop: 24
   }
 });
